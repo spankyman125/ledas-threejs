34156f141cfb604c4ee7f992bd3284dc49204c17
spankyman125 <spankyman@live.ru>
2025-01-30 14:00:25 +0500

    Cube render (Pure ThreeJS)

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,35 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.getElementById('root').appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x0ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+cube.rotateX(10)
+cube.rotateY(10)
+
+function animate() {
+
+  // cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
